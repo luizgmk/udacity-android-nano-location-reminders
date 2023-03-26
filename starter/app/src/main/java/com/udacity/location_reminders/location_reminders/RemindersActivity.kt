84b8b0659d86e23456094f9366ddcd8bfdbed8ad
Luiz Kimel <luiz@drivemode.com>
2023-03-26 15:11:53 +0900

    rename project, first pass on packages and get it to run. app running.

---
+++ starter/app/src/main/java/com/udacity/location_reminders/location_reminders/RemindersActivity.kt
@@ -0,0 +1,32 @@
+package com.udacity.location_reminders.location_reminders
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import com.udacity.location_reminders.R
+
+/**
+ * The RemindersActivity that holds the reminders fragments
+ */
+class RemindersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_reminders)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val view = item.actionView
+        if (view != null) {
+            when (item.itemId) {
+                android.R.id.home -> {
+                    Navigation.findNavController(view).popBackStack()
+                    return true
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
